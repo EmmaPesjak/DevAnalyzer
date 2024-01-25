abb7b8b68231438b1bd795e34f384d439fe308f1
ebbanimer <76969012+ebbanimer@users.noreply.github.com>
2024-01-25 11:34:26 +0100

    git commit -m "Root and menu created." -m "Created a base UI with an options-menu."

--- views/main_view.py
+++ views/main_view.py
@@ -1,3 +1,19 @@
+import customtkinter as ctk
+
 class MainView:
+    def __init__(self):
+        self.root = ctk.CTk()
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.root.title("DevAnalyzer")
+        self.root.geometry("900x600")
+        self.menubar = ctk.CTkOptionMenu(self.root, values=["Test1", "Test2", "Test3"])
+        self.menubar.set("Options")
+        self.menubar.pack()
+
+        self.root.mainloop()
+
+
     def display_data(self, data):
         print(data)
