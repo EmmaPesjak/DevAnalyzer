190a7b7ec5e9c0a1711117ba29ef008c05f112ed
Emma Pesjak <empe2105@student.miun.se>
2024-02-01 10:22:17 +0100

    Layout
    Feature:Configured a grid layout to have one side menu bar and one expandable window for the rest of the application.

--- views/main_view.py
+++ views/main_view.py
@@ -1,3 +1,4 @@
+import customtkinter
 import customtkinter as ctk
 from tkinter import messagebox
 
@@ -9,12 +10,26 @@ class MainView:
     def setup_ui(self):
         self.root.title("DevAnalyzer")
         self.root.geometry("900x600")
-        self.menubar = ctk.CTkOptionMenu(self.root, values=["Test1", "Test2", "Test3"])
+        self.root.resizable(width=True, height=True)
+
+        self.root.grid_rowconfigure(0, weight=1)  # This makes the row 0 expandable (fills the height).
+
+        self.root.grid_columnconfigure(0, minsize=200)  # This sets the minimum width for column 0 to 200.
+        self.root.grid_columnconfigure(1, weight=1)  # This makes column 1 expandable (fills the rest of the width).
+
+        frame1 = customtkinter.CTkFrame(self.root, border_width=5, border_color="gray", fg_color="blue")
+        frame1.grid(row=0, column=0, sticky="ns")  # 'ns' makes it expand only vertically.
+
+        frame2 = customtkinter.CTkFrame(self.root, fg_color="red")
+        frame2.grid(row=0, column=1, sticky="nsew")  # 'nsew' makes it expand in all directions.
+
+        users = ["Anna", "Clara", "Stina"]
+        self.menubar = ctk.CTkOptionMenu(frame1, values=users, command=self.usermenu_callback)
         self.menubar.set("Options")
         self.menubar.pack()
 
         # Exit button.
-        self.exit_button = ctk.CTkButton(self.root, text="Exit", command=self.on_closing)
+        self.exit_button = ctk.CTkButton(frame1, text="Exit", command=self.on_closing)
         self.exit_button.pack()
 
         # Exit handling.
@@ -23,6 +38,8 @@ class MainView:
         # Mainloop.
         self.root.mainloop()
 
+    def usermenu_callback(self, choice):
+        print("optionmenu dropdown clicked:", choice)
 
     def display_data(self, data):
         print(data)
