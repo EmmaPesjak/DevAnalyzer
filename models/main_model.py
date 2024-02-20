a026fdb7fa1922016b3052a9182a1ddcaef10696
ebbanimer <76969012+ebbanimer@users.noreply.github.com>
2024-02-20 08:49:40 +0100

    Pydriller set up
    Set up PyDriller and retrieved data from repository path

--- models/main_model.py
+++ models/main_model.py
@@ -1,3 +1,9 @@
+from pydriller import Repository
+
+for commit in Repository("https://github.com/EmmaPesjak/DevAnalyzer").traverse_commits():
+    print("Author: " + commit.author.name + ", msg: " + commit.msg)
+
 class MainModel:
+
     def get_data(self):
         return "Hi from the Model!"
