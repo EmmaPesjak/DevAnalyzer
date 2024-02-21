628909fd296a151beb4f8af5029b27eeafaba3e7
ebbanimer <76969012+ebbanimer@users.noreply.github.com>
2024-02-21 11:57:05 +0100

    Feature
    Retrieves correct amount of commits for project

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -10,5 +10,9 @@ class MainController:
     # TODO get the input, verify it, and send it to the model
     def retrieve_url(self, new_url):
         self.model.set_repo(new_url)
-        self.model.get_authors()
+        self.model.print_total_commits()
+        #self.model.save_to_json('repo_data.json')
+        #self.model.get_authors()
+        #self.model.get_commits_by_author('ebbanimer')
+        #self.model.get_total_amount_of_commits()
         #print(new_url)
--- models/main_model.py
+++ models/main_model.py
@@ -1,23 +1,75 @@
 from pydriller import Repository
-from urllib3.util import url
+from pydriller.metrics.process.commits_count import CommitsCount
+import json
+from pydriller.metrics.process.contributors_count import ContributorsCount
+
+
+
+# TODO
+# get info from repo;
+# contributors with all its commits
+# 1. store it in a file with; author: commits[]
+# 2. to get author names, loop through the keys of the dictionary
+# 3. to get total amount of commits, count the size of each list mapped to the author
+# 4. to get author with amount of commits, get author key and size of commit list
+# 5. later; NLP categorize
 
 
 class MainModel:
 
     def __init__(self):
         self.repo = None
+        self.author_commits = {}  # Store authors and their commits
+        self.authors = set()
 
-    def set_repo(self, url):
-        self.repo = Repository(url)
+    """Sets the repo url."""
+    def set_repo(self, repo_url):
+        self.repo = Repository(repo_url)
+        self.process_commits()
 
-    def retrieve_repository(self, url):
-        for commit in Repository(url).traverse_commits():
-            print("Author: " + commit.author.name + ", msg: " + commit.msg)
+    def process_commits(self):
+        for commit in self.repo.traverse_commits():
+            author_email = commit.author.email.lower()  # Using email as unique identifier
+            if author_email not in self.author_commits:
+                self.author_commits[author_email] = []
+            self.author_commits[author_email].append(commit.msg)
 
+        #print(self.author_commits)
+
+    def print_total_commits(self):
+        total_commits = sum(len(commits) for commits in self.author_commits.values())
+        print("Total number of commits:", total_commits)
+
+    def save_to_json(self, filename):
+        with open(filename, 'w') as file:
+            json.dump(self.author_commits, file, indent=4)
+
+
+
+
+
+
+    """Gets all authors for the repository."""
     def get_authors(self):
         for commit in self.repo.traverse_commits():
-            print("Author: " + commit.author.name)
 
+            author_identity = commit.author.email.lower()  # Using email as a unique identifier
+            self.authors.add(author_identity)
+
+        print(list(self.authors))
+
+    """Gets total amount of commits."""
+    def get_total_amount_of_commits(self):
+        metric = CommitsCount(path_to_repo=self.repo)
+        files = metric.count()
+        print('Files: {}'.format(files))
+        pass
+
+    """Gets total amount of commits for contributor."""
+    def get_commits_by_author(self, author_name):
+        for commit in self.repo.traverse_commits():
+            if commit.author.name == author_name:
+                print(commit.msg)
 
     def get_data(self):
         return "Hi from the Model!"
