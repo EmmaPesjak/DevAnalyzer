e255709b4ed8301f13e9b4af690cdb58eba5a4a4
Emma Pesjak <empe2105@student.miun.se>
2024-02-27 11:20:13 +0100

    Feature: added preprocessing of data

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -1,7 +1,8 @@
 class MainController:
-    def __init__(self, model, view):
-        self.model = model
+    def __init__(self, main_model, view, commit_analyzer):
+        self.main_model = main_model
         self.view = view
+        self.commit_analyzer = commit_analyzer
         self.view.set_on_input_change(self.retrieve_url)
 
     def run_app(self):
@@ -9,22 +10,25 @@ class MainController:
 
     # TODO get the input, verify it, and send it to the model
     def retrieve_url(self, new_url):
-        self.model.cleanup()
-        self.model.set_repo(new_url)
-        commits = self.model.get_total_amount_of_commits()
+        self.main_model.cleanup()
+        self.main_model.set_repo(new_url)
+        commits = self.main_model.get_total_amount_of_commits()
         print("Amount of commits: " + str(commits))
 
-        contributors = self.model.get_all_contributors()
+        contributors = self.main_model.get_all_contributors()
         print("Contributors: " + str(contributors))
 
-        most_active_month = self.model.get_most_active_month()
+        most_active_month = self.main_model.get_most_active_month()
         print("Most active month: " + str(most_active_month))
 
-        author_info = self.model.get_commit_data_with_files_for_author('ebba.nimer@gmail.com')
+        author_info = self.main_model.get_commit_data_with_files_for_author('ebba.nimer@gmail.com')
         print("Commit data for ebba.nimer@gmail.com: " + str(author_info))
 
-        activity_info = self.model.get_all_months_activity()
+        activity_info = self.main_model.get_all_months_activity()
         print("All months activity: " + str(activity_info))
 
-        all_commits = self.model.get_all_commits()
+        all_commits = self.main_model.get_all_commits()
         print("All commits: " + str(all_commits))
+
+        print("-----")
+        self.commit_analyzer.preprocess_data(all_commits)
--- models/commit_analyzer.py
+++ models/commit_analyzer.py
@@ -1,3 +1,4 @@
+import spacy
 
 class CommitAnalyzer:
     def __init__(self):
@@ -7,3 +8,39 @@ class CommitAnalyzer:
 
     """TODO:
     NLP analyzing"""
+
+    # SpaCy first step - preprocess data - get rid of all information that will not be used for the final output
+    def preprocess_data(self, all_commits):
+        nlp = spacy.load("en_core_web_sm")
+
+        for commit in all_commits:
+
+            # Check if the commit contains "Merge pull request".
+            if "Merge pull request" in commit:
+                print("Skipping: Contains 'Merge pull request'")
+                print("-------\n")
+                continue  # Skip this commit- skip och sortera automatiskt som merge commit???
+
+            doc = nlp(commit)
+            #print(f"Original Commit Message: {commit}")
+
+            # Adding custom stopwords.
+            nlp.Defaults.stop_words.add("\n\n")
+
+            # Filtering out stopwords, punctuation, numbers, and duplicates.
+            filtered_tokens = set(
+                [token.lemma_ for token in doc if not token.is_stop and not token.is_punct and not token.like_num])
+
+            print(f"Filtered Tokens: {filtered_tokens}")
+            print("-------\n")
+
+
+
+    # Gensim vectorizing text
+
+    # POS-tagging
+    # NER-tagging
+    # Dependency parsing
+
+    # Topic model
+    # Clustering and classification
