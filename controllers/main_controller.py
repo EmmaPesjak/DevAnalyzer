4cdfa10a6df0f72b52a9c71cfb2d738635ba1bda
Emma Pesjak <empe2105@student.miun.se>
2024-03-09 08:54:36 +0100

    fixed bug where error message when wrong repo url was entererd, also fixed better handling of the loading indicator and removal of user select.

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -19,7 +19,10 @@ class MainController:
     def handle_set_repo_result(self, result, error):
         if error:
             # Handle error, possibly in the main thread
-            self.view.show_error_message(str(error))
+            self.view.remove_loading_indicator()
+            self.view.remove_user_select()
+            self.view.show_init_label()
+            self.view.show_error_message("The repository URL was not valid, please try again with a valid one.")
         else:
             # Proceed with UI update or further data processing
             if self.main_model.write_to_file():
@@ -31,46 +34,3 @@ class MainController:
                 self.analyzer.analyze_commits(all_commits)
 
                 self.view.root.after(0, self.view.update_ui_after_fetch)
-            else:
-                # Handle calculation error
-                pass
-
-    # def retrieve_url(self, new_url):
-    #     self.main_model.cleanup()
-    #     result = self.main_model.set_repo(new_url)
-    #
-    #     # TODO stop UI update in view
-    #     if result != "Success":
-    #         self.view.show_error_message(
-    #             result)
-    #     else:
-    #
-    #         if self.main_model.calc_data():
-    #             self.view.root.after(0, self.view.update_ui_after_fetch)
-    #         else:
-    #             #Felmeddelande
-    #             pass
-    #
-    #         commits = self.main_model.get_total_amount_of_commits()
-    #         #print("Amount of commits: " + str(commits))
-    #
-    #         contributors = self.main_model.get_all_contributors()
-    #         #print("Contributors: " + str(contributors))
-    #
-    #         most_active_month = self.main_model.get_most_active_month()
-    #         #print("Most active month: " + str(most_active_month))
-    #
-    #         author_info = self.main_model.get_commit_data_with_files_for_author('ebba.nimer@gmail.com')
-    #         #print("Commit data for ebba.nimer@gmail.com: " + str(author_info))
-    #
-    #         activity_info = self.main_model.get_all_months_activity()
-    #         print("All months activity: " + str(activity_info))
-    #
-    #         all_commits = self.main_model.get_all_commits()
-    #         #print("All commits: " + str(all_commits))
-    #
-    #         #print("-----")
-    #         self.commit_analyzer.nlp(all_commits)
-    #
-
-
