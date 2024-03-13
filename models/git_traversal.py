0d2e65768d880c9ebda8ef28f0d63103f31f7c86
Emma Pesjak <empe2105@student.miun.se>
2024-03-13 15:14:07 +0100

    Added comment descriptions in the git_traversal class

--- models/git_traversal.py
+++ models/git_traversal.py
@@ -2,12 +2,23 @@ from pydriller import Repository, repository
 
 
 class GitTraversal:
+    """
+    Facilitates traversal and analysis of a Git repository using PyDriller.
+    """
 
     def __init__(self):
+        """
+        Initializes the GitTraversal object with no repository set.
+        """
         self.repo_url = None
         self.repo = None
 
     def set_repo(self, repo_url):
+        """
+        Sets the repository to the specified URL and initializes the Repository object.
+        :param repo_url: The URL of the Git repository.
+        :return: A string indicating "Success" or an error message if the repository cannot be set.
+        """
         try:
             self.repo = repository.Repository(repo_url)
             self.repo_url = repo_url
@@ -18,6 +29,10 @@ class GitTraversal:
         return error_message
 
     def get_authors_with_amount_of_commits(self):
+        """
+        Computes the number of commits made by each author in the repository.
+        :return: A dictionary mapping author names to their respective commit counts.
+        """
         total_commits_by_contributor = {}
 
         for commit in self.repo.traverse_commits():
