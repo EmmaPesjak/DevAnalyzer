69c39da6eb6733c43c64ec4f8d88a4f6d9831d08
Emma Pesjak <empe2105@student.miun.se>
2024-04-24 15:08:55 +0200

    changed epochs!

--- transformers_model/bert_filepath_model.py
+++ transformers_model/bert_filepath_model.py
@@ -144,8 +144,9 @@ for i, seed_value in enumerate(seed_values):
         per_device_eval_batch_size=32,
         warmup_steps=100,
         weight_decay=0.01,
-        evaluation_strategy="epoch",
-        save_strategy="epoch",
+        evaluation_strategy="steps",
+        eval_steps=50,
+        save_strategy="steps",
         fp16=False,
         load_best_model_at_end=True
     )
